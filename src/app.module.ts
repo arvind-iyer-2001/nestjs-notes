e78cb8f7f0d456577198aed3dd0cf192599d799b
arvindiyer <arvind.iyer@accionlabs.com>
2025-07-07 14:56:12 +0530

    fix: Fixed Dependency Injection Issues

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { NotesService } from './notes/notes.service';
 import { NotesModule } from './notes/notes.module';
 
 @Module({
   imports: [UserModule, NotesModule],
   controllers: [AppController],
-  providers: [AppService, NotesService],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/notes/notes.module.ts
+++ src/notes/notes.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { PrismaService } from '../prisma.service';
 import { NotesController } from './notes.controller';
+import { NotesService } from './notes.service';
 
 @Module({
-  providers: [UserService, PrismaService],
+  providers: [NotesService, UserService, PrismaService],
   controllers: [NotesController],
 })
 export class NotesModule {}
